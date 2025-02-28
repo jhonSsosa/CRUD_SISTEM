4fb76643aa328f17c0b4dfa792062e0a7b2aaab8
jhon.sosa-m <jhon.sosa-m@labinfo.is.escuelaing.edu.co>
2025-02-28 09:52:57 -0500

    first commit

---
+++ complete/src/main/java/com/example/accessingdatajpa/Property.java
@@ -0,0 +1,59 @@
+package com.example.accessingdatajpa;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "properties")
+public class Property {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String address;
+    private Double price;
+    private Double size;
+    private String description;
+
+    public Property() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Double getSize() {
+        return size;
+    }
+
+    public void setSize(Double size) {
+        this.size = size;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
